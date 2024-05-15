e4b250147e214860b2c33825de4c2f40305ef096
Deyson Venté <deiandresvent@hotmail.com>
2024-05-15 00:42:19 -0500

    Modulos para crear el sidebar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatListModule,
+  ],
 })
-export class MaterialModule { }
+export class MaterialModule {}
