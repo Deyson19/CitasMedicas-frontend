5326051545afd723ecef76160bf76ddd445658d1
Deyson Venté <deiandresvent@hotmail.com>
2024-05-15 16:32:42 -0500

    Ruteo principal configurado, cargar el respectivo componente y sus rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pacientes',
+    loadChildren: () =>
+      import('./pacientes/pacientes.module').then((m) => m.PacientesModule),
+  },
+  {
+    path: 'medicos',
+    loadChildren: () =>
+      import('./medicos/medicos.module').then((m) => m.MedicosModule),
+  },
+  {
+    path: 'citas-medicas',
+    loadChildren: () =>
+      import('./citas-medicas/citas-medicas.module').then(
+        (x) => x.CitasMedicasModule
+      ),
+  },
+  {
+    path: 'historial-medico',
+    loadChildren: () =>
+      import('./historial-medico/historial-medico.module').then(
+        (x) => x.HistorialMedicoModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'pacientes',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
