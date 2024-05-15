d46a00d7c78f1ad3c8de0168163c5b88eabfb149
Deyson Venté <deiandresvent@hotmail.com>
2024-05-15 16:28:52 -0500

    pagina para ver el historial por paciente

---
+++ src/app/pacientes/pages/historial-paciente/historial-paciente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './historial-paciente.component.html',
+  styleUrls: ['./historial-paciente.component.css']
+})
+export class HistorialPacienteComponent {
+
+}
