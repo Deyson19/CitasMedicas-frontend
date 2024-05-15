d1c911c6b4f032a1cdb58e7052bdd1cad295e04b
Deyson Venté <deiandresvent@hotmail.com>
2024-05-15 16:28:26 -0500

    Pagina para buscar pacientes

---
+++ src/app/pacientes/pages/buscador/buscador.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './buscador.component.html',
+  styleUrls: ['./buscador.component.css']
+})
+export class BuscadorComponent {
+
+}
