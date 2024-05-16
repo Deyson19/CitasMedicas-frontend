93e0deb52e46d12d2d8d929950c81b74c195cf24
Deyson Venté <deiandresvent@hotmail.com>
2024-05-15 20:42:12 -0500

    servicio para las peticiones al controlador de pacientes

---
+++ src/app/pacientes/services/pacientes.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit, computed, inject, signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environments';
+import {
+  PacientesResponse,
+  PacienteResponse,
+  HistorialPaciente,
+  Pacientes,
+} from '../interfaces/';
+import { CrearActualizarPaciente } from '../models/crearpaciente.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PacientesService implements OnInit {
+  private _http = inject(HttpClient);
+  private _baseUrl = environment.apiUrl;
+  constructor() {}
+  ngOnInit(): void {}
+
+  getPacientes(): Observable<PacientesResponse> {
+    return this._http.get<PacientesResponse>(`${this._baseUrl}/Paciente`);
+  }
+
+  getPaciente(idPaciente: string): Observable<PacienteResponse> {
+    return this._http.get<PacienteResponse>(
+      `${this._baseUrl}/Paciente/${idPaciente}`
+    );
+  }
+
+  getHistorialPaciente(idPaciente: string): Observable<HistorialPaciente> {
+    return this._http.get<HistorialPaciente>(
+      `${this._baseUrl}/Paciente/HistorialPaciente/${idPaciente}`
+    );
+  }
+
+  searchPaciente(nombre: string): Observable<PacientesResponse> {
+    return this._http.get<PacientesResponse>(
+      `${this._baseUrl}/Paciente/search/${nombre}`
+    );
+  }
+
+  postCrearPaciente(
+    paciente: CrearActualizarPaciente
+  ): Observable<PacienteResponse> {
+    return this._http.post<PacienteResponse>(
+      `${this._baseUrl}/Paciente`,
+      paciente
+    );
+  }
+  putActualizarPaciente(
+    paciente: CrearActualizarPaciente
+  ): Observable<PacienteResponse> {
+    return this._http.put<PacienteResponse>(
+      `${this._baseUrl}/Paciente`,
+      paciente
+    );
+  }
+}
