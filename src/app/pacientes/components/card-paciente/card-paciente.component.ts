e325ef3ae9c3fe72d2e21c61e43ed10b486ef135
Deyson Venté <deiandresvent@hotmail.com>
2024-05-15 22:42:35 -0500

    Card para mostrar el paciente. Dialog,toast y servicios inyectados

--- src/app/pacientes/components/card-paciente/card-paciente.component.ts
+++ src/app/pacientes/components/card-paciente/card-paciente.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Paciente } from '../../interfaces';
+import { ToastrService } from 'ngx-toastr';
+import { PacientesService } from '../../services/pacientes.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
+import { filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pacientes-card-paciente',
@@ -8,4 +13,29 @@ import { Paciente } from '../../interfaces';
 })
 export class CardPacienteComponent {
   @Input({ required: true }) paciente!: Paciente;
+  private dialog = inject(MatDialog);
+  private _toast = inject(ToastrService);
+  private _pacientesService = inject(PacientesService);
+
+  onDeletePaciente() {
+    if (!this.paciente.id) {
+      this._toast.error('No hay paciente para eliminar', 'UUPPS!');
+    }
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: true,
+    });
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter((result) => result),
+        switchMap(() => this._pacientesService.deletePaciente(this.paciente.id))
+      )
+      .subscribe((x) => {
+        if (x.isSuccess) {
+          this._toast.warning(x.message, 'OK!');
+        }
+      });
+  }
 }
