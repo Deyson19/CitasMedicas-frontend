29067c771ae50b3adf0761342d972322d7135b5f
Deyson Venté <deiandresvent@hotmail.com>
2024-05-15 20:41:39 -0500

    Listado de pacientes ok

--- src/app/pacientes/pages/listado/listado.component.ts
+++ src/app/pacientes/pages/listado/listado.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { PacientesService } from '../../services/pacientes.service';
+import { Pacientes } from '../../interfaces';
 
 @Component({
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit {
+  private _pacientesService = inject(PacientesService);
+  pacientes: Pacientes[] = [];
+  private listadoPacientes = signal<Pacientes[]>([]);
 
+  ngOnInit(): void {
+    this._pacientesService.getPacientes().subscribe((x) => {
+      if (x.isSuccess) {
+        this.listadoPacientes.set(x.result);
+        this.pacientes = this.listadoPacientes();
+      }
+    });
+  }
 }
