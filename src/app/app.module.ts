ebecfd7824b4c5eab0c947509914073c1d39aff9
Deyson Venté <deiandresvent@hotmail.com>
2024-05-15 20:41:08 -0500

    Modulos para realizar peticiones y trabajar con formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { SpinnerComponent, SharedModule, NavbarComponent } from './shared/';
-
+import { SpinnerComponent, SharedModule } from './shared/';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -15,6 +16,9 @@ import { SpinnerComponent, SharedModule, NavbarComponent } from './shared/';
     BrowserAnimationsModule,
     SpinnerComponent,
     SharedModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     ToastrModule.forRoot({
       positionClass: 'toast-top-right',
       preventDuplicates: true,
