33fadc8d512a033b216b326763fa0ada4069ede0
Deyson Venté <deiandresvent@hotmail.com>
2024-05-17 13:10:34 -0500

    Se ha mejorado el modelo para recibir la petición del historial medico.

--- src/app/pacientes/interfaces/index.ts
+++ src/app/pacientes/interfaces/index.ts
@@ -1,3 +1,3 @@
+export { HistorialPacienteResponse } from './historial-pacientes.interface';
 export * from '../interfaces/pacientes.interface';
 export * from '../interfaces/paciente.interface';
-export * from '../interfaces/historial-pacientes.interface';
