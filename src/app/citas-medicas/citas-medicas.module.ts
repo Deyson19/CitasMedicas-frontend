deece2dc08cdd99e4f704dda2849112b79ee1ea3
Deyson Venté <deiandresvent@hotmail.com>
2024-05-18 12:36:02 -0500

    Routermodule, sharedmodule y materialmodule importados

--- src/app/citas-medicas/citas-medicas.module.ts
+++ src/app/citas-medicas/citas-medicas.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { IndexComponent } from './layout/index.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared';
+import { MaterialModule } from '../material/material.module';
+import { ListadoComponent } from './pages/';
+import { CitasMedicasRoutingModule } from './citas-medicas-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [IndexComponent, ListadoComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MaterialModule,
+    CitasMedicasRoutingModule,
+  ],
 })
-export class CitasMedicasModule { }
+export class CitasMedicasModule {}
