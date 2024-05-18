df0a1977739cb6cc82603ee2cdb634585a17906a
Deyson Venté <deiandresvent@hotmail.com>
2024-05-18 14:34:22 -0500

    Importar el spinner y sharedmodule

--- src/app/citas-medicas/citas-medicas.module.ts
+++ src/app/citas-medicas/citas-medicas.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './layout/index.component';
 import { RouterModule } from '@angular/router';
-import { SharedModule } from '../shared';
+import { SharedModule, SpinnerComponent } from '../shared';
 import { MaterialModule } from '../material/material.module';
 import { ListadoComponent } from './pages/';
 import { CitasMedicasRoutingModule } from './citas-medicas-routing.module';
+import { CitaComponent } from './components/cita/cita.component';
 
 @NgModule({
-  declarations: [IndexComponent, ListadoComponent],
+  declarations: [IndexComponent, ListadoComponent, CitaComponent],
   imports: [
     CommonModule,
     RouterModule,
     SharedModule,
     MaterialModule,
     CitasMedicasRoutingModule,
+    SpinnerComponent,
   ],
 })
 export class CitasMedicasModule {}
