9bab9efd126baf865f70dc1730a1408d64951da9
Deyson Venté <deiandresvent@hotmail.com>
2024-05-18 12:36:48 -0500

    Ruta cabecera del modulo configurada

---
+++ src/app/citas-medicas/layout/index.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-citas-index',
+  templateUrl: './index.component.html',
+})
+export class IndexComponent {
+  constructor() {
+    console.log('Isloading');
+  }
+}
