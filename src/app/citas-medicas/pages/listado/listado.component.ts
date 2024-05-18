80a10bc7fedbff326daeab4ceddbb95da7d786fb
Deyson Venté <deiandresvent@hotmail.com>
2024-05-18 14:35:44 -0500

    listar las citas médicas en el sistema.

--- src/app/citas-medicas/pages/listado/listado.component.ts
+++ src/app/citas-medicas/pages/listado/listado.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { CitasMedicasService } from '../../services/citas-medicas.service';
+import { CitaMedica } from 'src/app/models';
+import { ToastrService } from 'ngx-toastr';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit {
+  //*Servicios
+  private _citasService = inject(CitasMedicasService);
+  private _snackBar = inject(MatSnackBar);
+  private _toast = inject(ToastrService);
+  //*Variables
+  public citasMedicas: CitaMedica[] = [];
+  mensaje = 'Cargando citas medicas';
+  isLoading = signal<boolean>(true);
 
+  ngOnInit(): void {
+    this._citasService.getAll().subscribe((resp) => {
+      if (resp.isSuccess) {
+        setTimeout(() => {
+          this.isLoading.set(false);
+          this._snackBar.open(resp.message, 'Ok', {
+            duration: 2500,
+          });
+        }, 1800);
+        this.citasMedicas = resp.result;
+      } else {
+        this._snackBar.open(resp.message, 'Ok', {
+          duration: 2000,
+        });
+      }
+    });
+  }
 }
