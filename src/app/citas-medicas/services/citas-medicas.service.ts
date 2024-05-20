019fd35a08ab5be9f19991a5ffa3e3bee6237bec
Deyson Venté <deiandresvent@hotmail.com>
2024-05-19 19:09:43 -0500

    Metodo para listar los medicos

--- src/app/citas-medicas/services/citas-medicas.service.ts
+++ src/app/citas-medicas/services/citas-medicas.service.ts
@@ -5,20 +5,24 @@ import { environment } from 'src/environments/environments';
 import { CitaMedicaResponse, CitasMedicasResponse } from '../interfaces';
 import { CrearCitaMedicaResponse } from '../interfaces/crear-cita-medica-response';
 import { CrearCitaMedica } from '../models/crear-cita-medica';
+import { Medico } from 'src/app/models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CitasMedicasService {
+  //TODO: Evaluar los casos de error en los components
   private _http = inject(HttpClient);
-  private _baseUrl = environment.apiUrl + '/CitaMedica';
+  private _baseUrl = environment.apiUrl;
   constructor() {}
 
   getAll(): Observable<CitasMedicasResponse> {
-    return this._http.get<CitasMedicasResponse>(`${this._baseUrl}`);
+    return this._http.get<CitasMedicasResponse>(`${this._baseUrl}/CitaMedica`);
   }
   getById(id: string): Observable<CitaMedicaResponse> {
-    return this._http.get<CitaMedicaResponse>(`${this._baseUrl}/${id}`);
+    return this._http.get<CitaMedicaResponse>(
+      `${this._baseUrl}/CitaMedica/${id}`
+    );
   }
   post(citaMedica: CrearCitaMedica): Observable<CrearCitaMedicaResponse> {
     return this._http.post<CrearCitaMedicaResponse>(
@@ -27,6 +31,18 @@ export class CitasMedicasService {
     );
   }
   delete(id: string): Observable<string> {
-    return this._http.delete<string>(`${this._baseUrl}/${id}`);
+    return this._http.delete<string>(`${this._baseUrl}/CitaMedica/${id}`);
   }
+
+  //! Este metodo no puede persistir aquí
+  //TODO: Una vez implementado el modulo de Medicos, se debe utilizar el listado que este proporciona
+
+  listadoMedicos(): Observable<MedicosRespose> {
+    return this._http.get<MedicosRespose>(`${this._baseUrl}/Medico`);
+  }
+}
+interface MedicosRespose {
+  isSuccess: boolean;
+  message: string;
+  result: Medico[];
 }
