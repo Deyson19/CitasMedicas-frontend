96183a5aa2a86cdb6645c2ee4603f2e6689b97cb
Deyson Venté <deiandresvent@hotmail.com>
2024-05-21 00:26:44 -0500

    Se ha mejorado la presentación y toast luego de eliminar registros.

--- src/app/citas-medicas/pages/detalle/detalle.component.ts
+++ src/app/citas-medicas/pages/detalle/detalle.component.ts
@@ -6,6 +6,7 @@ import { CitaMedica } from 'src/app/models';
 import { CitasMedicasService } from '../../services/citas-medicas.service';
 import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
 import { filter, switchMap } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   templateUrl: './detalle.component.html',
@@ -15,7 +16,6 @@ export class DetalleComponent implements OnInit {
   private _router = inject(Router);
   private _activedRouter = inject(ActivatedRoute);
   private _citasService = inject(CitasMedicasService);
-  private _toastService = inject(ToastrService);
   private dialog = inject(MatDialog);
   private _toast = inject(ToastrService);
   //*Variables
@@ -35,21 +35,26 @@ export class DetalleComponent implements OnInit {
       }, 2000);
       return;
     }
-    this._citasService.getById(citaId).subscribe((resp) => {
-      if (resp.isSuccess) {
-        setTimeout(() => {
-          this.citaMedica = resp.result;
-          this.isLoading = false;
-        }, 1800);
-        this._toast.info(resp.message, 'OK', {
-          timeOut: 1500,
-        });
+    this._citasService.getById(citaId).subscribe(
+      (resp) => {
+        if (resp.isSuccess) {
+          setTimeout(() => {
+            this.citaMedica = resp.result;
+            this.isLoading = false;
+          }, 1800);
+          this._toast.info(resp.message, 'OK', {
+            timeOut: 1500,
+          });
+        }
+      },
+      (error) => {
+        this._toast.error(error.error, 'Error');
+        this._router.navigate(['/citas-medicas']);
       }
-    });
+    );
   }
 
   onDelete() {
-    console.log(this.citaMedica);
     if (!this.citaMedica!.id) {
       this._toast.error('No hay elemento para eliminar', 'Incorrecto!');
     }
@@ -64,15 +69,22 @@ export class DetalleComponent implements OnInit {
         filter((result) => result),
         switchMap(() => this._citasService.delete(this.citaMedica!.id))
       )
-      .subscribe((x) => {
-        if (x) {
-          this._toast.warning(x, 'Realizado');
-          setTimeout(() => {
-            this._router.navigate(['/citas-medicas']);
-          }, 1500);
-        } else {
-          this._toast.error(x, 'Error');
+      .subscribe(
+        (x) => {
+          console.log(x);
+          if (x) {
+            this._toast.warning(x, 'Realizado');
+            setTimeout(() => {
+              this._router.navigate(['/citas-medicas']);
+            }, 1500);
+          } else {
+            this._toast.error(x, 'Error');
+          }
+        },
+        (error: HttpErrorResponse) => {
+          this._toast.warning(error.error.text, 'Error');
+          this._router.navigate(['/citas-medicas']);
         }
-      });
+      );
   }
 }
