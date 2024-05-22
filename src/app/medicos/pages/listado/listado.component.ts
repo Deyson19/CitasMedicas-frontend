3e982f38c06c16694cdb4ae39b628e0271ca7b57
Deyson Venté <deiandresvent@hotmail.com>
2024-05-22 15:48:09 -0500

    Listado de medicos

---
+++ src/app/medicos/pages/listado/listado.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medicos-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css'],
+})
+export class ListadoComponent {}
