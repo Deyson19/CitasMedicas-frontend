42286ed437b5842954dace2083e87720f560172f
Deyson Venté <deiandresvent@hotmail.com>
2024-05-22 15:47:47 -0500

    component cabecera de la ruta medicos.

---
+++ src/app/medicos/layout/layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medicos-layout',
+  template: ` <p>layout works!</p>
+    <router-outlet />`,
+})
+export class LayoutComponent {}
