fae9f84d716566780d6f56e1c0365446c5e1044d
Deyson Venté <deiandresvent@hotmail.com>
2024-05-22 17:08:04 -0500

    Listado de médicos ok

--- src/app/medicos/pages/listado/listado.component.ts
+++ src/app/medicos/pages/listado/listado.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { MedicosService } from '../../services/medicos.service';
+import { Medico } from 'src/app/models';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-medicos-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css'],
 })
-export class ListadoComponent {}
+export class ListadoComponent implements OnInit {
+  private _toast = inject(ToastrService);
+  private _medicos = inject(MedicosService);
+  public medicosList: Medico[] = [];
+  isLoading = true;
+  mensaje = 'Listando los Médicos';
+
+  ngOnInit() {
+    this.listado();
+  }
+  private listado() {
+    this._medicos.obtenerTodos().subscribe(
+      (resp) => {
+        if (resp.isSuccess) {
+          setTimeout(() => {
+            this.isLoading = false;
+            this.medicosList = resp.result;
+            this._toast.success(resp.message, 'Resultados', {
+              timeOut: 3000,
+            });
+          }, 1500);
+        }
+      },
+      (isError: HttpErrorResponse) => {
+        this.mensaje = 'No hay elementos para mostrar';
+        this._toast.error(isError.message, 'Error');
+      }
+    );
+  }
+}
