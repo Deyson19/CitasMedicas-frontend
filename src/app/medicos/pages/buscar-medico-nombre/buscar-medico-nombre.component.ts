c8158bbc2e424f4e9a6a81f50610547068b4a98a
Deyson Venté <deiandresvent@hotmail.com>
2024-05-22 15:48:26 -0500

    Buscar medico por nombre

---
+++ src/app/medicos/pages/buscar-medico-nombre/buscar-medico-nombre.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medicos-buscar-medico-nombre',
+  templateUrl: './buscar-medico-nombre.component.html',
+})
+export class BuscarMedicoNombreComponent {}
