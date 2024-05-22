fbe10081f250709456e9bfabee1295715215c1b4
Deyson Venté <deiandresvent@hotmail.com>
2024-05-22 15:49:55 -0500

    Archivo de barril para pages

---
+++ src/app/medicos/pages/index.ts
@@ -0,0 +1,5 @@
+export { BuscarMedicoEspecialidadComponent } from './buscar-medico-especialidad/buscar-medico-especialidad.component';
+export { BuscarMedicoNombreComponent } from './buscar-medico-nombre/buscar-medico-nombre.component';
+export { DetalleComponent } from './detalle/detalle.component';
+export { ListadoComponent } from './listado/listado.component';
+export { UpsertComponent } from './upsert/upsert.component';
