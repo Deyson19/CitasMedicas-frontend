1404fdc4643a1ffaf01e6b9bd03157265c0e2c93
Deyson Venté <deiandresvent@hotmail.com>
2024-05-23 15:54:29 -0500

    Servicio para las peticiones http creados.

---
+++ src/app/historial-medico/services/historial-medico.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environments';
+import {
+  CrearHistorial,
+  CrearHistorialResponse,
+  HistorialMedicoResponse,
+  HistorialesMedicosResponse,
+} from '../interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HistorialMedicoService {
+  private _http = inject(HttpClient);
+  private _baseUrl = environment.apiUrl;
+
+  obtenerTodos(): Observable<HistorialesMedicosResponse> {
+    return this._http.get<HistorialesMedicosResponse>(
+      `${this._baseUrl}/HistorialMedico`
+    );
+  }
+  obtener(idHistorial: string): Observable<HistorialMedicoResponse> {
+    return this._http.get<HistorialMedicoResponse>(
+      `${this._baseUrl}/HistorialMedico/${idHistorial}`
+    );
+  }
+  crear(historial: CrearHistorial): Observable<CrearHistorialResponse> {
+    return this._http.post<CrearHistorialResponse>(
+      `${this._baseUrl}/HistorialMedico`,
+      historial
+    );
+  }
+  eliminar(historialId: string): Observable<string> {
+    return this._http.delete<string>(
+      `${this._baseUrl}/HistorialMedico/${historialId}`
+    );
+  }
+  historialPorMedico(medicoId: string): Observable<HistorialMedicoResponse> {
+    return this._http.get<HistorialMedicoResponse>(
+      `${this._baseUrl}/HistorialMedico/MedicoId/${medicoId}`
+    );
+  }
+  historialPorPaciente(
+    pacienteId: string
+  ): Observable<HistorialMedicoResponse> {
+    return this._http.get<HistorialMedicoResponse>(
+      `${this._baseUrl}/HistorialMedico/PacienteId/${pacienteId}`
+    );
+  }
+}
