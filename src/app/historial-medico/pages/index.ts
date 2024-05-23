dad570982548210d72b74ef4c4525f302e042751
Deyson Venté <deiandresvent@hotmail.com>
2024-05-23 14:48:09 -0500

    Archivo de barril de pages

---
+++ src/app/historial-medico/pages/index.ts
@@ -0,0 +1,5 @@
+export { DetalleComponent } from './detalle/detalle.component';
+export { HistorialMedicoComponent } from './historial-medico/historial-medico.component';
+export { HistorialPacienteComponent } from './historial-paciente/historial-paciente.component';
+export { ListPageComponent } from './list-page/list-page.component';
+export { UpsertComponent } from './upsert/upsert.component';
