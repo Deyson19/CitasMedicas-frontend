31b5e8564a355bd85ddae673a99ce3480971473b
Deyson Venté <deiandresvent@hotmail.com>
2024-05-23 14:46:43 -0500

    Historial del médico

---
+++ src/app/historial-medico/pages/historial-medico/historial-medico.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './historial-medico.component.html',
+  styleUrls: ['./historial-medico.component.css']
+})
+export class HistorialMedicoComponent {
+
+}
