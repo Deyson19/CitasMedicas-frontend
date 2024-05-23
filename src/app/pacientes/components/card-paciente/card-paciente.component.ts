89e1467ecee7c275dd0abfa17e8c22383f691241
Deyson Venté <deiandresvent@hotmail.com>
2024-05-23 13:59:19 -0500

    Emitir un valor una vez eliminado el registro.

--- src/app/pacientes/components/card-paciente/card-paciente.component.ts
+++ src/app/pacientes/components/card-paciente/card-paciente.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { PacientesService } from '../../services/pacientes.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
 import { filter, switchMap } from 'rxjs/operators';
 import { Paciente } from 'src/app/models';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-pacientes-card-paciente',
@@ -16,6 +17,9 @@ export class CardPacienteComponent {
   private dialog = inject(MatDialog);
   private _toast = inject(ToastrService);
   private _pacientesService = inject(PacientesService);
+
+  @Output()
+  wasDelete = new EventEmitter<boolean>();
 
   onDeletePaciente() {
     if (!this.paciente.id) {
@@ -32,10 +36,21 @@ export class CardPacienteComponent {
         filter((result) => result),
         switchMap(() => this._pacientesService.deletePaciente(this.paciente.id))
       )
-      .subscribe((x) => {
-        if (x.isSuccess) {
-          this._toast.warning(x.message, 'OK!');
+      .subscribe(
+        (x) => {
+          if (x.isSuccess) {
+            this._toast.warning(x.message, 'OK!');
+            this.wasDelete.emit(true);
+          } else {
+            this._toast.error(
+              x.message ? x.message : 'No se pudo eliminar el paciente',
+              'UUPPS!'
+            );
+          }
+        },
+        (error: HttpErrorResponse) => {
+          this._toast.error('No se pudo eliminar el paciente', 'Error!');
         }
-      });
+      );
   }
 }
