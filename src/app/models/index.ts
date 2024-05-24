156debe49d203b65ba8ef5c224cceef5801c9167
Deyson Venté <deiandresvent@hotmail.com>
2024-05-23 20:23:36 -0500

    interface para menu de navegacion

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,3 +1,4 @@
+export { MenuItems } from './menuitems.interface';
 export { HistorialMedico } from './historial-medico.interface';
 export { Medico } from './medico.interface';
 export { CitaMedica } from './cita-medica.interface';
---
+++ src/app/models/menuitems.interface.ts
@@ -0,0 +1,5 @@
+export interface MenuItems {
+  path: string;
+  name: string;
+  icon?: string;
+}
